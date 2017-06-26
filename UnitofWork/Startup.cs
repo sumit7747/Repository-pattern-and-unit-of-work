447dc2a8e0d4c2e501e0107d739c0c09373f1408
sumit7747 <sumit7747@gmail.com>
2017-06-26 13:19:41 +0530

    Add project files.

---
+++ UnitofWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnitofWork.Startup))]
+namespace UnitofWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
